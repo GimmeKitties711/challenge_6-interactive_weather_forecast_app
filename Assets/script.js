8260d4acc34daade78624ae72b7facd9e3f55200
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-10-19 17:22:47 -0700

    got fetching weather data to work

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,56 @@
 let APIkey = "3ea7e44fe8cae8888a2fcecf8667f496";
 
 function fetchWeatherData(lat, lon) {
-    fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`)
-    .then(resp => resp.json())
-    .then(json => console.log(json));
+    console.log('fetching weather data')
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`)
+    .then(resp => {
+        console.log(resp)
+        return resp.json()
+    })
+    .then(json => makeForecast(json.list));
+}
+
+function makeForecast(data) {
+    let newData = new Array(0);
+    for (let i = 0; i < data.length; i+=8) {
+        newData.push(data[i]);
+    }
+    console.log("newData: ", newData);
 }
 
 function fetchGeoCoordinates(city_name, limit) {
+    console.log('fetching geo coordinates')
+    console.log ("city name: ", city_name, ", limit: ", limit)
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=${limit}&appid=${APIkey}`)
-    .then(resp => resp.json())
-    .then(json => function() {
-        return json[0].lat, json[0].lon;
-    }); //console.log('typeof json[0].lat: ' + typeof json[0].lat + ', typeof json[0].lon: ' + typeof json[0].lon));
+    .then(resp =>{
+        console.log(resp)
+        return resp.json()})
+    .then(json => {
+        console.log(json)
+    
+        fetchWeatherData(json[0].lat, json[0].lon)
+        
+        // return [json[0].lat, json[0].lon]
+        
+    }
+        //console.log('json[0].lat: ', json[0].lat, ', json[0].lon: ', json[0].lon)
+        // return {
+        //     latitude: json[0].lat,
+        //     longitude: json[0].lon
+        // };
+    ); //console.log('typeof json[0].lat: ' + typeof json[0].lat + ', typeof json[0].lon: ' + typeof json[0].lon));
 
     //console.log('json[0]: ', json[0]);
 }
 
-let lat;
-let lon;
-lat, lon = fetchGeoCoordinates("Chicago", 5);
-console.log('lat: ' + lat + ', lon: ' + lon);
+//fetchGeoCoordinates("Chicago", 5);
+//fetchWeatherData(fetchGeoCoordinates("Chicago", 5));
+//let [latitude, longitude] = fetchGeoCoordinates("Chicago", 5);
+//fetchGeoCoordinates("Chicago", 5);
+//console.log('latitude: ', latitude, ', longitude: ', longitude);
+//console.log(fetchGeoCoordinates("Chicago", 5));
+//let {lat, lon} = fetchGeoCoordinates("Chicago", 5);
+//console.log('lat: ', lat, ', lon: ', lon);
 //console.log('latitude: ', latAndLon);
 //console.log('longitude: ', latAndLon);
 
@@ -50,3 +81,23 @@ function fetchCatFact() {
         .then(json => fact.textContent = json.data); // sets the text content of the element with the id "cat-fact" to the data retrieved by fetching. the fact element is empty until the random cat and fact button is clicked.
 }
 */
+
+// function getValue(name, age) {
+//     console.log(name, age)
+// }
+// var username = getValue(["John", 25], true);
+// console.log(username)
+
+function getStoredCities() {
+    let storedCities = JSON.parse(localStorage.getItem("cities"));
+    if (storedCities === null) {
+        storedCities = [];
+    }
+    return storedCities;
+}
+
+function saveNewCity(city) {
+    let storedCities = getStoredCities();
+    storedCities.push(city);
+    localStorage.setItem("cities", JSON.stringify(storedCities));
+}
