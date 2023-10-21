3b323d7754100597fccac52b29a6f03e20c242bb
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-10-20 23:56:22 -0700

    made some styling changes

--- Assets/script.js
+++ Assets/script.js
@@ -68,7 +68,7 @@ function fetchGeoCoordinates(city_name, limit) {
     //console.log('json[0]: ', json[0]);
 }
 
-fetchGeoCoordinates("Kansas City", 10);
+fetchGeoCoordinates("San Francisco", 10);
 //fetchWeatherData(fetchGeoCoordinates("Chicago", 5));
 //let [latitude, longitude] = fetchGeoCoordinates("Chicago", 5);
 //fetchGeoCoordinates("Chicago", 5);
@@ -81,42 +81,49 @@ fetchGeoCoordinates("Kansas City", 10);
 
 let cityName = document.getElementById("city-name");
 let currentDate = document.getElementById("current-date");
+let currentWeatherDescription = document.getElementById("current-weather-description");
 let currentWeatherIcon = document.getElementById("current-weather-icon");
 let currentTemperature = document.getElementById("current-temperature");
 let currentWind = document.getElementById("current-wind");
 let currentHumidity = document.getElementById("current-humidity");
 
 let dateOneDayOut = document.getElementById("date-one-day-out");
+let descriptionOneDayOut = document.getElementById("description-one-day-out");
 let weatherIconOneDayOut = document.getElementById("weather-icon-one-day-out");
 let temperatureOneDayOut = document.getElementById("temperature-one-day-out");
 let windOneDayOut = document.getElementById("wind-one-day-out");
 let humidityOneDayOut = document.getElementById("humidity-one-day-out");
 
 let dateTwoDaysOut = document.getElementById("date-two-days-out");
+let descriptionTwoDaysOut = document.getElementById("description-two-days-out");
 let weatherIconTwoDaysOut = document.getElementById("weather-icon-two-days-out");
 let temperatureTwoDaysOut = document.getElementById("temperature-two-days-out");
 let windTwoDaysOut = document.getElementById("wind-two-days-out");
 let humidityTwoDaysOut = document.getElementById("humidity-two-days-out");
 
 let dateThreeDaysOut = document.getElementById("date-three-days-out");
+let descriptionThreeDaysOut = document.getElementById("description-three-days-out");
 let weatherIconThreeDaysOut = document.getElementById("weather-icon-three-days-out");
 let temperatureThreeDaysOut = document.getElementById("temperature-three-days-out");
 let windThreeDaysOut = document.getElementById("wind-three-days-out");
 let humidityThreeDaysOut = document.getElementById("humidity-three-days-out");
 
 let dateFourDaysOut = document.getElementById("date-four-days-out");
+let descriptionFourDaysOut = document.getElementById("description-four-days-out");
 let weatherIconFourDaysOut = document.getElementById("weather-icon-four-days-out");
 let temperatureFourDaysOut = document.getElementById("temperature-four-days-out");
 let windFourDaysOut = document.getElementById("wind-four-days-out");
 let humidityFourDaysOut = document.getElementById("humidity-four-days-out");
 
 let dateFiveDaysOut = document.getElementById("date-five-days-out");
+let descriptionFiveDaysOut = document.getElementById("description-five-days-out");
 let weatherIconFiveDaysOut = document.getElementById("weather-icon-five-days-out");
 let temperatureFiveDaysOut = document.getElementById("temperature-five-days-out");
 let windFiveDaysOut = document.getElementById("wind-five-days-out");
 let humidityFiveDaysOut = document.getElementById("humidity-five-days-out");
 
 let futureDates = [dateOneDayOut, dateTwoDaysOut, dateThreeDaysOut, dateFourDaysOut, dateFiveDaysOut];
+let futureDescriptions = [descriptionOneDayOut, descriptionTwoDaysOut, descriptionThreeDaysOut, descriptionFourDaysOut, descriptionFiveDaysOut];
 let futureWeatherIcons = [weatherIconOneDayOut, weatherIconTwoDaysOut, weatherIconThreeDaysOut, weatherIconFourDaysOut, weatherIconFiveDaysOut];
 let futureTemperatures = [temperatureOneDayOut, temperatureTwoDaysOut, temperatureThreeDaysOut, temperatureFourDaysOut, temperatureFiveDaysOut];
 let futureWinds = [windOneDayOut, windTwoDaysOut, windThreeDaysOut, windFourDaysOut, windFiveDaysOut];
@@ -127,10 +134,11 @@ function populateCurrentWeather(object) {
     currentDate.textContent = dayjs().format('M/DD/YYYY');
     // source for how to get current date in dayjs(): https://day.js.org/docs/en/parse/now
     // source for how to format date in dayjs(): https://day.js.org/docs/en/display/format
+    currentWeatherDescription.textContent = object.weather[0].description;
     let weatherIcon = object.weather[0].icon;
-    let weatherIconURL = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+    let weatherIconURL = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
     currentWeatherIcon.setAttribute('src', weatherIconURL);
-    //currentWeatherIcon.textContent = selectWeatherIcon(object.weather[0].description);
+    // source for weather icons, their meaning, and how to use them: https://openweathermap.org/weather-conditions
     currentTemperature.textContent = convertTemperature(object.main.temp).toFixed(2); // toFixed(2) rounds to two decimal places
     // source for how to round a number to two decimal places: https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
     currentWind.textContent = object.wind.speed.toFixed(2);
@@ -168,8 +176,9 @@ function populateFutureWeather(object) {
 
     for (let i = 0; i < object.length; i++) {
         futureDates[i].textContent = dayjs().add(i+1, 'day').format('M/DD/YYYY');
+        futureDescriptions[i].textContent = object[i].weather[0].description;
         weatherIcon = object[i].weather[0].icon;
-        weatherIconURL = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+        weatherIconURL = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
         futureWeatherIcons[i].setAttribute('src', weatherIconURL);
         futureTemperatures[i].textContent = convertTemperature(object[i].main.temp).toFixed(2);
         futureWinds[i].textContent = object[i].wind.speed.toFixed(2);
